55e5e0e97c2baba88492307c96f97b6fabf7aa3a
ekaterinakalina <kalina.katharina@gmail.com>
2023-06-02 11:14:17 +0200

    XL --> XXL, changed product size

--- js/data.js
+++ js/data.js
@@ -17,7 +17,7 @@ export const data = {
             description:
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
             image: './img/product2',
-            sizes: ['M', 'L', 'XL'],
+            sizes: ['M', 'L', 'XXXL'],
             stock: 220,
         },
         {
