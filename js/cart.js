d75173a45c6a952761dd7c0f34f42b71c4ee911d
Robert <disaro@gmail.com>
2023-06-07 16:26:07 +0200

    cart updates

--- js/cart.js
+++ js/cart.js
@@ -3,7 +3,7 @@ import { el, addAmount } from './utils.js';
 // Define cartObj
 const cartObj = {
     productArr: [],
-    totalItems: '',
+    totalItems: 0,
     totalPrice: '',
 };
 
@@ -15,24 +15,27 @@ const cartObj = {
 // Define the die addToCart-Funktion
 export function addToCart(itemObj) {
     const existingProductIndex = cartObj.productArr.findIndex(
-        (product) => product.itemId === itemObj.itemId && product.itemSize === itemObj.itemSize
-      );
-    
-      if (existingProductIndex !== -1) {
+        (product) =>
+            product.itemId === itemObj.itemId &&
+            product.itemSize === itemObj.itemSize
+    );
+
+    if (existingProductIndex !== -1) {
         const existingProduct = cartObj.productArr[existingProductIndex];
-        const newAmount = parseInt(existingProduct.itemAmount) + parseInt(itemObj.itemAmount);
+        const newAmount =
+            parseInt(existingProduct.itemAmount) + parseInt(itemObj.itemAmount);
         const itemStock = parseInt(existingProduct.itemStock);
-      
+
         if (newAmount <= itemStock) {
-          existingProduct.itemAmount = newAmount.toString();
+            existingProduct.itemAmount = newAmount.toString();
         } else {
-          existingProduct.itemAmount = itemStock.toString();
+            existingProduct.itemAmount = itemStock.toString();
         }
-      } else {
+    } else {
         cartObj.productArr.push(itemObj);
       }
       createCart(cartObj.productArr);
-      
+      updateAmount();
       // Updates the number of items in the cart; calculate the total price
       updateAmountPrice()
 }
@@ -43,16 +46,20 @@ function createCart(cart) {
 
     cart.forEach((value) => {
 
-        // Template for products in the shopping cart
+        // Template for products in the shopping cart     
         cartTempl += `
         <div class="summary-info">
         <div class="flex-row">
           <img src="${value.itemImg}">
           <h3>${value.itemName}</h3>
           <div class="summary-size">${value.itemSize}</div>
-          <div class="summary-amount">${addAmount(value.itemStock, value.itemAmount)}</div>
+          <div class="summary-amount" data-previous-value="${
+              value.itemAmount
+          }">${addAmount(value.itemStock, value.itemAmount)}</div>
           <div class="price">${value.itemPrice} €</div>
-          <button data-id="${value.itemId}" data-size="${value.itemSize}" class="remove-btn">X</button> 
+          <button data-id="${value.itemId}" data-size="${
+            value.itemSize
+        }" class="remove-btn">X</button>
         </div>
         <hr>
       </div>
@@ -74,18 +81,65 @@ function createCart(cart) {
 
     // Event Listeners for remove button
     const removeButtons = document.querySelectorAll('.remove-btn');
+    const summaryAmountSelects = document.querySelectorAll('.summary-amount');
+
     removeButtons.forEach((button) => {
         button.addEventListener('click', handleDeleteItem);
     });
 
+    summaryAmountSelects.forEach((select) => {
+        select.addEventListener('change', selectAmount);
+    });
+
     // console.log(cart);
-    // console.log(cartObj.productArr);
+    console.log(cartObj);
 }
 
 /**
  * Function that checks if Product is already in cart and only increases the quantity
  * TODO: like description
  */
+
+function selectAmount(e) {
+    const select = e.target;
+    const selectedValue = parseInt(select.value);
+    const previousValue =
+        parseInt(select.parentNode.getAttribute('data-previous-value')) ||
+        parseInt(select.dataset.previousValue);
+
+    console.log(cartObj.totalItems, 'cartObj.productArr.totalItems');
+
+    if (selectedValue > previousValue) {
+        // Wert wurde erhöht
+        const difference = selectedValue - previousValue;
+        console.log(`Wert um ${difference} erhöht`);
+        console.dir(cartObj);
+        cartObj.totalItems += difference;
+        updateAmount(true);
+
+        // Führe die entsprechende Aktion aus
+    } else if (selectedValue < previousValue) {
+        // Wert wurde verringert
+        const difference = previousValue - selectedValue;
+        console.log(`Wert um ${difference} verringert`);
+        cartObj.totalItems -= difference;
+        updateAmount(true);
+        console.log(cartObj);
+        // Führe die entsprechende Aktion aus
+    } else {
+        // Wert wurde nicht geändert
+        console.log('Wert wurde nicht geändert');
+        // Führe keine Aktion aus
+    }
+
+    console.log(cartObj.totalItems, 'cartObj.productArr.totalItems after');
+
+    // Only initially
+    select.parentNode.removeAttribute('data-previous-value');
+
+    // Aktualisiere den vorherigen Wert im Dataset des Select-Felds
+    select.dataset.previousValue = selectedValue;
+}
 
 /**
  * Function that deletes a cart object
@@ -109,12 +163,17 @@ function handleDeleteItem(e) {
     // Recreate updated cart
     createCart(cartObj.productArr);
     // Updates the number of items in the cart; calculate the total price
+    updateAmount();
     updateAmountPrice();
 }
 
 /**
  * Function that updates the quantity of products next to the cart symbol;
- * Calculates and updates the total price
+ *Calculates and updates the total price
+ */
+/**
+ * Function that updates the quantity of products next to the cart symbol
+ *
  */
 function updateAmountPrice() {
     // Total items 
@@ -136,3 +195,4 @@ function updateAmountPrice() {
         el('#cart-amount-text').innerText = cartObj.totalItems 
         el('#subtotal-text').innerHTML = cartObj.totalPrice;
 }
+
