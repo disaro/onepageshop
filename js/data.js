b19b058b676581f282df4cd7cb0cfdf84bb3d6cc
Md Mahabur Rahaman <121830061+mahabur-rahaman@users.noreply.github.com>
2023-06-07 13:01:49 +0200

    adding real img

--- js/data.js
+++ js/data.js
@@ -5,7 +5,7 @@ export const data = {
             name: 'Product 1',
             price: 10.99,
             description:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+            "Our t-shirt is the epitome of comfort, featuring a relaxed fit and soft, breathable fabric that feels like a dream against your skin. It's the perfect choice for casual outings or ",
             image: 'article1',
             sizes: ['S', 'M', 'L'],
             stock: 23,
@@ -13,9 +13,9 @@ export const data = {
         {
             id: 2,
             name: 'Product 2',
-            price: 21,
+            price: 21.99,
             description:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+             "While trends come and go, our t-shirt offers timeless appeal. It's a wardrobe staple that transcends seasons, making it a smart investment that will remain stylish for years to come.",
             image: 'article2',
             sizes: ['M', 'L', 'XL'],
             stock: 0,
@@ -25,7 +25,7 @@ export const data = {
             name: 'Product 3',
             price: 10.99,
             description:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+             "We believe in responsible manufacturing practices, which is why our t-shirt is ethically produced. You can feel good about your purchase, knowing that it supports fair labor and ",
             image: 'article3',
             sizes: ['S', 'M', 'XL'],
             stock: 97,
@@ -33,12 +33,34 @@ export const data = {
         {
             id: 4,
             name: 'Product 4',
-            price: 9,
+            price: 9.99,
             description:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+            " Your clothing is an extension of your personality, and our t-shirt allows you to express yourself effortlessly. Choose from a variety of designs that resonate with your interests, ",
+
             image: 'article4',
             sizes: ['L', 'XL'],
             stock: 56,
+        },
+        {
+            id: 5,
+            name: 'Product 5',
+            price: 12.99,
+            description:
+            "While trends come and go, our t-shirt offers timeless appeal. It's a wardrobe staple that transcends seasons and fads, making it a smart investment that will remain stylish for years to come.",
+
+            image: 'article6',
+            sizes: ['S','L', 'XL'],
+            stock: 32,
+        },
+        {
+            id: 6,
+            name: 'Product 6',
+            price: 9.99,
+            description:
+            "Designed for the long haul, our t-shirt is made to withstand the rigors of everyday wear. It's tough, yet comfortable, so you can rely on it to keep up with your  compromising on style.",
+            image: 'article5',
+            sizes: ['S','L', 'XL'],
+            stock: 26,
         },
     ],
 };
